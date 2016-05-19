e3a8b87ec3b5282f026485a9cdfb53e58e6e497e
johtani <johtani@DESKTOP-C1Q5CRO>
2016-05-19 18:45:49 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApplication1/NestSample.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Nest;
+
+
+namespace DecodeSample
+{
+    class NestSample
+    {
+
+        static void DebugPrint(string s)
+        {
+            Debug.WriteLine(s);
+        }
+
+        static void Main(string[] args)
+        {
+            var indexName = "sample_index";
+            var typeName = "document";
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node)
+                .DefaultIndex(indexName)
+                .PrettyJson();
+            var client = new ElasticClient(settings);
+
+            //indexing
+            var doc2 = new SampleDocument
+            {
+                Id = 2,
+                Title = "はじめてのElasticsearch",
+                Body = "NESTで始める、はじめてのElasticsearchクライアントアプリケーション"
+            };
+            client.Index<SampleDocument>(doc2, idx => idx.Index(indexName).Type(typeName));
+
+            DebugPrint("--- indexing sample doc2");
+
+            //Searching
+            var searchDescriptor = new SearchDescriptor<SampleDocument>()
+                .Query(q => q.MatchAll())
+                .Index(indexName)
+                .Type(typeName);
+            DebugPrint("--- match_all start -------------------");
+
+            var response = client.Search<SampleDocument>(searchDescriptor);
+            DebugPrint("Hits : [" + response.Total + "]");
+            DebugPrint("Docs : [" + response.Documents.Count() + "]");
+            foreach (var doc in response.Documents)
+            {
+                DebugPrint(doc.ToString());
+            }
+            DebugPrint("--- match_all end -------------------");
+
+            DebugPrint("---  query start ------------------ - ");
+            searchDescriptor.Query(
+                q => q.QueryString(
+                    qs => qs.Query(
+                        "body:NEST"
+                    )
+                )
+            );
+            response = client.Search<SampleDocument>(searchDescriptor);
+            DebugPrint("Hits : [" + response.Total + "]");
+            DebugPrint("Docs : [" + response.Documents.Count() + "]");
+            foreach (var doc in response.Documents)
+            {
+                DebugPrint(doc.ToString());
+            }
+            DebugPrint("--- query_string query end -------------------");
+
+
+        }
+    }
+
+    class SampleDocument
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + "{ id : " + Id + ", title : " + Title + ", body : " + Body + " }";
+        }
+
+    }
+}
---
+++ DotNetClientSampleApp/IndexAndSearchSample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Elasticsearch.Net;
+
+namespace DecodeSample
+{
+    class IndexAndSearchSample
+    {
+        static void Main(string[] args)
+        {
+            var node = new Uri("http://localhost:9200");
+            var config = new ConnectionConfiguration(node).PrettyJson();
+            var client = new ElasticLowLevelClient(config);
+
+            //Indexing
+            var doc1 = new {
+                id = 1,
+                title = "はじめてのElasticsearch", 
+                body = ".NETで始める、はじめてのElasticsearchクライアントアプリケーションの説明"
+            };
+            
+            client.Index<String>("sample_index", "document", "1", doc1);
+            client.IndicesFlush<String>("sample_index");
+
+            //Searching
+            var queryJson = @"{ ""query"" : { ""match_all"": {}} }";
+            DebugPrint("--- match_all start -------------------");
+            ElasticsearchResponse<String> res = client.Search<String>("sample_index", queryJson);
+
+            DebugPrint(res.Body);
+            DebugPrint("--- match_all end -------------------");
+
+            DebugPrint("--- query_string query start -------------------");
+            queryJson = @"{ ""query"" : { ""query_string"": { ""query"" : ""body:.NET"" } } }";
+
+            res = client.Search<String>("sample_index", queryJson);
+            DebugPrint(res.Body);
+            DebugPrint("--- query_string query end -------------------");
+
+        }
+
+        static void DebugPrint(string s)
+        {
+            Debug.WriteLine(s);
+        }
+    }
+}
